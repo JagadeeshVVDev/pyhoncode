f7bfdfb0485dae4a59b1349243bfba9c9fa72756
JagadeeshVVDev <124361258+JagadeeshVVDev@users.noreply.github.com>
2023-03-25 14:48:54 +0530

    Add files via upload

---
+++ day_2.py
@@ -0,0 +1,15 @@
+# Day_2 Take an input character from user and check wheather it is an alphabet or not
+
+  # fisrtway
+# c=input("Enter a Character :")
+# if(c.isdigit()):   #c.isalpha()
+#     print("Not an alphabet")
+# else:
+#     print('Alphabet')
+
+    #another way
+c=input("Enter a Character :")
+if("A"<=c<='Z'or 'a'<=c<='z'):
+    print('Alphabet')
+else:
+    print("not an alphabet")
